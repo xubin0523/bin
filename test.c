83d0eaa0d770947400bc17a794ab976f348dcb0b
xubin0523@yeah.net <xubin@xubin-VirtualBox.(none)>
2013-03-12 13:51:30 +0800

    test

---
+++ test.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: xubin
+	> Mail: xubin0523@163.com 
+	> Created Time: Tue 12 Mar 2013 12:34:55 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+	printf("hello,world!\n");
+	return 0;
+}
